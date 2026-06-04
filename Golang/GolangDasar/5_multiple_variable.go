37c177685ab372deac660db32eda3f8838a032bf
MikhaelEmpi <d.mikhaelempi@gmail.com>
2026-06-04 17:32:10 +0700

    material: multiple variable

---
+++ Golang/GolangDasar/5_multiple_variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		firstName  = "Joko"
+		middleName = "Noto"
+		lastName   = "Boto"
+	)
+
+	fmt.Println("Nama saya", firstName, middleName, lastName)
+}
